216e7fd5743e96a9dcc0a82412c4e508bd566852
Daniel Aguilar <daniel.aguilar@pakke.com>
2024-04-24 21:01:43 +0200

    Character cards

--- src/app/components/pages/characters/characters-card/characters-card.component.ts
+++ src/app/components/pages/characters/characters-card/characters-card.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { Character } from '@app/shared/interfaces/data.interface';
 
 @Component({
   selector: 'app-characters-card',
   templateUrl: './characters-card.component.html',
-  styleUrls: ['./characters-card.component.scss']
+  styleUrls: ['./characters-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CharactersCardComponent implements OnInit {
+  @Input() character: Character;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toggleFavorite(): void {
+    const isFavorite = this.character.isFavorite;
+    this.getIcon();
+    this.character.isFavorite = !isFavorite;
   }
 
+  getIcon(): string {
+    return this.character.isFavorite ? 'heart-solid.svg' : 'heart.svg';
+  }
 }
